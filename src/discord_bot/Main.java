6849b20fc28b769db2d3067fd74fa680c9318ccb
Nenad <32616777+NenadGvozdenac@users.noreply.github.com>
2022-03-28 17:22:06 +0000

    Github messed up the formatting...

--- src/discord_bot/Main.java
+++ src/discord_bot/Main.java
@@ -18,9 +18,11 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
 
-public class Main {																		// Main Class
+public class Main {																		
+  // Main klasa
 
-	public static JDA jda;																// Bot
+	public static JDA jda;																
+  // Bot
 	
 	public static String[] messages = 
 		{
@@ -28,50 +30,77 @@ public class Main {																		// Main Class
 			"a guitar", 
 			"idk", 
 			"with some bitches..."
-		};																				// Statusne poruke
-	public static int currentIndex = 0;													// Index poruke
+		};																				
+  // Statusne poruke
+  
+	public static int currentIndex = 0;													
+  // Index poruke
 	
 	public static void main(String[] args) throws LoginException, 
-									InterruptedException, IOException {					// Main funkcija
+									InterruptedException, IOException {					
+    // Main funkcija
 	
 		// -- Vadjenja tokena --
 		
-		File token_fajl = new File("token.txt");										// Fajl za token, gde se cuva
-    	Scanner tR = new Scanner(token_fajl);											// Scanner za token
+		File token_fajl = new File("token.txt");										
+    // Fajl za token, gde se cuva
+    
+    Scanner tR = new Scanner(token_fajl);											
+    // Scanner za token
 
-    	String token = tR.nextLine();													// Uzimamo token iz fajla
+    String token = tR.nextLine();													
+    // Uzimamo token iz fajla
     	
-    	tR.close();																		// Zavrsavamo, izlaz iz fajla
+    tR.close();	
+    // Zavrsavamo, izlaz iz fajla
     	
-    	// -- Instanciranje objekta -- 
+    // -- Instanciranje objekta -- 
 	
-		jda = JDABuilder.createDefault(token).build();									// Instanciranje JDA objekta, sa tokenom
+		jda = JDABuilder.createDefault(token).build();
+    // Instanciranje JDA objekta, sa tokenom
 		
 		// -- Aktivnost, status, onlineStatus --
 		
-		new Timer().schedule(new TimerTask(){											// Timer za menjanje statusne poruke
-		  public void run(){															// Funkcija za rad, async
+		new Timer().schedule(new TimerTask() {	
+      // Timer za menjanje statusne poruke
+		  public void run() {	
+        // Funkcija za rad, async
 		    jda.getPresence().setActivity(Activity.playing(messages[currentIndex]));	// Menjanje statusa na osnovu indexa
-		    currentIndex=(currentIndex+1)%messages.length;								// Povecavanje indexa
-		  }},0,30_000);																	// Svakih 30 sekundi
+		    currentIndex=(currentIndex+1)%messages.length;  
+        // Povecavanje indexa
+		  }},0,30_000);	
+    // Svakih 30 sekundi
 		
-		jda.getPresence().setStatus(OnlineStatus.IDLE);									// OnlineStatus stavljamo na IDLE
+		jda.getPresence().setStatus(OnlineStatus.IDLE);	
+    // OnlineStatus stavljamo na IDLE
 		
 		// -- Listeneri --
 		
-		jda.addEventListener(new Commands());											// Dodavanje event listenera
-		jda.addEventListener(new OtherEvents());										// Klase Commands i JoinEvent
+		jda.addEventListener(new Commands());	
+    // Dodavanje event listenera
+    
+		jda.addEventListener(new OtherEvents());	
+    // Klase Commands i JoinEvent
 		
-		jda.awaitReady();																// Cekamo pocetak
+		jda.awaitReady();	
+    // Cekamo pocetak
 		
-		jda.setAutoReconnect(true);														// U slucaju diskonekta, ponovo se ukljuci
+		jda.setAutoReconnect(true);	
+    // U slucaju diskonekta, ponovo se ukljuci
 		
 		// -- Odrzavanje bota aktivnim --
 		
-		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);			// Aktiviramo server na portu 8
-        server.createContext("/test", new MyHandler());									// Pravi handle za nastavak URL-a, /test
-        server.setExecutor(null); 														// Executor stavljamo na nista
-        server.start();																	// Pocinjemo server
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);			
+    // Aktiviramo server na portu 8
+    
+    server.createContext("/test", new MyHandler());	
+    // Pravi handle za nastavak URL-a, /test
+    
+    server.setExecutor(null); 
+    // Executor stavljamo na nista
+    
+    server.start();	
+    // Pocinjemo server
 
 	}
 }
